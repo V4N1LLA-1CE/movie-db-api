f5d00611d117cca81f5c37e8522c2ab1280cadab
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-11-20 22:51:55 +1100

    refactor: abstract parse id parameter into app helper

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // POST /v1/movies
@@ -15,11 +12,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 // GET /v1/movies/:id
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// get slice of query params from request
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// parse :id field in query param into int64
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := app.readIdParam(r)
 	if err != nil {
 		http.NotFound(w, r)
 		return
