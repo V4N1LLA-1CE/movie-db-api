0b753b0865fb9c5ae7e9939fe9c84aed4fcc1fa0
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-11-21 19:12:58 +1100

    feat: write json helper + healthcheck responds with json now

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // GET /v1/healthcheck
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: alive")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// map of response
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process the request", http.StatusInternalServerError)
+	}
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -13,10 +14,36 @@ import (
 func (app *application) readIdParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
+	// parse id into int64
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
 
 	return id, nil
+}
+
+// helper to write json to response
+// returns json response body that has been written and the error if there is any
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	json, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// better formatting for terminal output i.e. curl
+	json = append(json, '\n')
+
+	// loop through header map and set key and value
+	for hkey, hval := range headers {
+		w.Header()[hkey] = hval
+	}
+
+	// set json header and status code
+	// then write response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(json)
+
+	return nil
 }
