3a3679fcbcc35a234993e19ce66b76729ed35172
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-03 19:16:17 +1100

    chore(db): optimise database by adding indexing

---
+++ db/migrations/000004_add_movies_indexes.up.sql
@@ -0,0 +1,3 @@
+-- add index on genres and title for better performance on searching
+CREATE INDEX IF NOT EXISTS movies_title_idx ON movies USING GIN (to_tsvector('simple', title));
+CREATE INDEX IF NOT EXISTS movies_genres_idx ON movies USING GIN (genres);
