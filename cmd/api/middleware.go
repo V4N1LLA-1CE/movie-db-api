a221b69b244e1e2cda7701e8afc50a1cf5657bb2
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-06 19:20:07 +1100

    feat(middleware): rate limiter now works for individual IPs/clients

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -25,19 +28,67 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
+// NOTE: This method of rate limiting will only work if API is hosted on a single machine
+// If infra is distributed with multiple servers and load balancer, it won't work
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// initialise new rate limiter
-	// rate limit: 2 - on average allows 2 events/operations per second
-	// burst size: 4 - max number of tokens in bucket (4 tokens max so 4 requests in quick succession)
-	limiter := rate.NewLimiter(2, 4)
+	// client struct for holding rate limiter and last seen time
+	type client struct {
+		limiter           *rate.Limiter
+		mostRecentRequest time.Time
+	}
+
+	// map of clients that get mapped by client IP addresses
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// background goroutine to cleanup clients every minute
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// lock for cleanup
+			mu.Lock()
+
+			// if client hasn't requested for 3 minutes, delete from map
+			for ip, client := range clients {
+				if time.Since(client.mostRecentRequest) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			// unlock mutex
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// everytime Allow() is called, 1 token will be consumed from bucket
-		// if no tokens, Allow() is false
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		// lock mutex to allow only 1 goroutine to read/write to clients map at a time
+		mu.Lock()
+
+		// check if ip exists in map, if not, add a new client and add to map with ip
+		if _, exists := clients[ip]; !exists {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+
+		// update client's most recent request
+		clients[ip].mostRecentRequest = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		// unlock mutex
+		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
