8b98893a3f6204ce09af6fe468e64ddcb6c247ed
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-07 15:03:55 +1100

    feat: add usermodel to model

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,11 +13,13 @@ var (
 // models struct wraps all models using a single container
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 	// TODO: Add more models here when needed
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
