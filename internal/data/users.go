f7f5f3ecc4afaaafbea7ed592f66735452071bd3
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-07 14:50:27 +1100

    refactor: validate user information

--- internal/data/users.go
+++ internal/data/users.go
@@ -4,18 +4,19 @@ import (
 	"errors"
 	"time"
 
+	"github.com/V4N1LLA-1CE/movie-db-api/internal/validator"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID           int64     `json:"id"`
-	CreatedAt    time.Time `json:"created_at"`
-	Name         string    `json:"name"`
-	Email        string    `json:"email"`
-	PasswordHash int64     `json:"-"`
-	Activated    bool      `json:"activated"`
-	Version      uuid.UUID `json:"version"`
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   uuid.UUID `json:"-"`
 }
 
 // use pointer to distinguish between nil and "" when there's no password
@@ -50,4 +51,31 @@ func (p *password) Matches(plaintext string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRegex), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 characters long")
+	v.Check(len(password) <= 72, "password", "must not exceed 72 characters long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not exceed 500 characters long")
+
+	ValidateEmail(v, user.Email)
+
+	// if plaintext pass is not nil, validate the plaintext
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
