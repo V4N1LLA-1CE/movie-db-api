99400f6a2db4efdba37b1f468fe879bdc0b35924
V4N1LLA-1CE <164747892+V4N1LLA-1CE@users.noreply.github.com>
2024-12-09 20:45:57 +1100

    feat: send email in background

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -56,17 +56,18 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	// send email
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	// send email in background
+	// log error if any
+	go func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			app.logger.Error(err.Error())
+		}
+	}()
 
 	// write response with newly created user details
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
-		return
 	}
 }
